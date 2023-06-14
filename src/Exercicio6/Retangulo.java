7884f952223d4376efebb3578c96fc5df92f7967
WellZup <wellington.oliveira@zup.com.br>
2023-06-14 20:16:25 -0300

    :recycle: Removendo gets sem chamadas

--- src/Exercicio6/Retangulo.java
+++ src/Exercicio6/Retangulo.java
@@ -4,21 +4,6 @@ public class Retangulo {
     double ladoA;
     double ladoB;
 
-    public double getLadoA() {
-        return ladoA;
-    }
-
-    public double getLadoB() {
-        return ladoB;
-    }
-
-    public void setLadoA(double ladoA) {
-        this.ladoA = ladoA;
-    }
-
-    public void setLadoB(double ladoB) {
-        this.ladoB = ladoB;
-    }
 
     public double calcularArea(){
         return ladoA * ladoB;
